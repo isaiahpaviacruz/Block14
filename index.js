f844d46206e88fd52fcd4ec57219ea23c12d994b
isaiahpaviacruz <141981406+isaiahpaviacruz@users.noreply.github.com>
2023-10-08 15:58:01 -0700

    add upload

---
+++ index.js
@@ -0,0 +1,128 @@
+// === DO NOT EDIT THIS REGION ===
+// Read the comments to understand how the program is structured.
+
+// Prompt the user for a list of integers separated by commas.
+const userInputString = prompt(
+  "Please enter some integers separated by commas.",
+  "1,2,3,4,5"
+);
+
+// Split the string of numbers into an array of strings.
+const stringArray = userInputString.split(",");
+
+// Convert the array of strings into an array of numbers.
+const numbers = [];
+for (let i = 0; i < stringArray.length; i++) {
+  const str = stringArray[i];
+  const number = parseInt(str);
+  numbers.push(number);
+}
+
+// Peform some calculations on the numbers.
+console.log(numbers);
+console.log(`You have given ${getLength(numbers)} numbers.`);
+console.log(`The sum of your numbers is ${getSum(numbers)}.`);
+console.log(`The mean of your numbers is ${getMean(numbers)}.`);
+console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
+console.log(`The largest of your numbers is ${getMax(numbers)}.`);
+console.log(`The range of your numbers is ${getRange(numbers)}.`);
+console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
+
+// === EDIT THE CODE BELOW ===
+// Complete the functions below to make the program work!
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the length of the array
+ */
+function getLength(numbers) {
+  // TODO
+// Define an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+
+// Calculate the sum of the numbers in the array
+// const numbers = [1, 2, 3, 4, 5];
+const sum = numbers.reduce((acc, current) => acc + current, 0);
+console.log(sum); // Output: 15
+
+}
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the sum of the numbers
+ */
+function getSum(numbers) {
+  // TODO
+  console.log(sum); // Output: 15
+}
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the mean of the numbers
+ */
+function getMean(numbers) {
+  // TODO
+  const mean = sum / numbers.length;
+  console.log(mean); // Output: 3
+}
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the smallest of the numbers
+ */
+function getMin(numbers) {
+  // TODO
+  const min = Math.min(...numbers);
+  console.log(min); // Output: 1
+}
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the largest of the numbers
+ */
+function getMax(numbers) {
+  // TODO
+
+  
+const max = Math.max(...numbers);
+
+console.log(max); // Output: 5
+
+}
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the range of the numbers (max - min)
+ */
+function getRange(numbers) {
+  // TODO
+const range = [];
+  for (let i = start; i <= end; i += step) {
+    range.push(i);
+  }
+  return range;
+
+}
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number[]} the even numbers in the array
+ */
+function getEvens(numbers) {
+  // TODO
+  const evenNumbers = numbers.filter(number => number % 2 === 0);
+
+console.log(evenNumbers); 
+}
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number[]} the odd numbers in the array
+ */
+function getOdds(numbers) {
+  // TODO
+  const oddNumbers = numbers.filter(number => number % 2 !== 0);
+
+console.log(oddNumbers);
+}
